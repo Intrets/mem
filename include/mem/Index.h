494cc08a688e1689b0e89bb3c505304e0057c1ed
Intrets <geen.inspiratie824@gmail.com>
2021-06-12 21:36:57 +0200

    removed ambiguous SizeAlias used for indexing both rawdata and everything structures, added Index<T> index type alias for clarity and safety

---
+++ include/mem/Index.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <cstdint>
+#include <concepts>
+
+#include <serial/Serializer.h>
+
+template<class T, std::integral index_type = size_t>
+struct Index
+{
+	index_type i;
+
+	operator index_type() const {
+		return i;
+	}
+
+	Index<T, index_type> operator++() {
+		++i;
+		return *this;
+	}
+
+	Index<T, index_type> operator++(int) {
+		auto pre = *this;
+		++i;
+		return pre;
+	}
+
+	Index<T, index_type> operator--() {
+		--i;
+		return *this;
+	}
+
+	Index<T, index_type> operator--(int) {
+		auto pre = *this;
+		--i;
+		return pre;
+	}
+
+	void set(index_type j) {
+		this->i = j;
+	}
+};
+
+template<class T>
+struct serial::Serializable<Index<T>>
+{
+	static constexpr std::string_view typaName = "Index";
+
+	ALL_DEF(Index<T>) {
+		return serializer.runAll<Selector>(
+			ALL(i)
+			);
+	}
+};
