f6dac680a29acb953af4b71e00d180e6de796902
Intrets <geen.inspiratie824@gmail.com>
2022-04-15 02:51:31 +0200

    removed unneeded misc dependency

--- include/mem/Everything.h
+++ include/mem/Everything.h
@@ -23,8 +23,6 @@
 #include <optional>
 #include <algorithm>
 
-#include <misc/Misc.h>
-
 #include <tepp/tepp.h>
 
 #include "Global.h"
@@ -35,6 +33,13 @@ struct Component;
 
 template<class T>
 struct Identifier;
+
+#define DEFAULT_COPY(T) T(const T&) = default; T& operator=(const T&) = default;
+#define NO_COPY(T) T(const T&) = delete; T& operator=(const T&) = delete;
+#define DEFAULT_MOVE(T) T(T&&) = default; T& operator=(T&&) = default;
+#define NO_MOVE(T) T(T&&) = delete; T& operator=(T&&) = delete;
+#define DEFAULT_COPY_MOVE(T) DEFAULT_COPY(T) DEFAULT_MOVE(T)
+#define NO_COPY_MOVE(T) NO_COPY(T) NO_MOVE(T)
 
 namespace mem
 {
